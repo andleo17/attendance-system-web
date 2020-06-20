3ee19b9e35b2d798db447df5045e46f9b4cb48c2
Andres Baldarrago <andres.gastulo@gmail.com>
2020-06-19 21:38:25 -0500

    Implementand marcar asistencia.

--- Server/Schema/Mutation.cs
+++ Server/Schema/Mutation.cs
@@ -1,4 +1,5 @@
 using HotChocolate;
+using HotChocolate.Execution;
 using Microsoft.EntityFrameworkCore;
 using Server.Models;
 using Server.Schema.Inputs;
@@ -10,6 +11,73 @@ namespace Server.Schema
 {
 	public class Mutation
 	{
+		public async Task<Attendance> AddAttendance([Service] DBAttendanceContext dBAttendanceContext, string employeeCardId)
+		{
+			try
+			{
+				var currentSchedule = await dBAttendanceContext.Schedule.SingleOrDefaultAsync(s => s.EmployeeCardId == employeeCardId && s.State);
+				if (currentSchedule != null)
+				{
+					var currentDay = await currentSchedule.ScheduleDetail.FirstOrDefaultAsync(sd => DateTime.Today.DayOfWeek.Equals(sd.Day));
+					if (currentDay != null)
+					{
+						var now = DateTime.Now.TimeOfDay;
+						var tolerance = TimeSpan.FromMinutes(10);
+						if (now.Subtract(tolerance) <= now)
+						{
+							var attendance = new Attendance
+							{
+								Date = DateTime.Today,
+								InHour = now,
+								EmployeeCardId = employeeCardId
+							};
+							dBAttendanceContext.Attendance.Add(attendance);
+							await dBAttendanceContext.SaveChangesAsync();
+							return attendance;
+						}
+						else
+						{
+							throw new QueryException("Aún no le toca marcar asistencia.");
+						}
+					}
+					else
+					{
+						throw new QueryException("Hoy no le toca marcar asistencia.");
+					}
+				}
+				else
+				{
+					throw new QueryException("Horario no encontrado.");
+				}
+			}
+			catch (System.Exception e)
+			{
+				throw new QueryException(e.Message);
+			}
+		}
+
+		public async Task<Attendance> OutAttendance([Service] DBAttendanceContext dBAttendanceContext, int attendanceId)
+		{
+			try
+			{
+				var attendance = await dBAttendanceContext.Attendance.FindAsync(attendanceId);
+				if (attendance != null)
+				{
+					attendance.OutHour = DateTime.Now.TimeOfDay;
+					await dBAttendanceContext.SaveChangesAsync();
+					return attendance;
+				}
+				else
+				{
+					throw new QueryException("Asistencia no encontrada.");
+				}
+			}
+			catch (System.Exception e)
+			{
+				throw new QueryException(e.Message);
+			}
+		}
+
 		public async Task<Models.LicenseType> AddLicenseType([Service] DBAttendanceContext dBAttendanceContext, AddLicenseTypeInput input)
 		{
 			try
@@ -27,22 +95,28 @@ namespace Server.Schema
 			}
 			catch (System.Exception e)
 			{
-				Console.WriteLine(e);
-				throw;
+				throw new QueryException(e.Message);
 			}
 		}
 
 		public async Task<User> AddUser([Service] DBAttendanceContext dBAttendanceContext, AddUserInput input)
 		{
-			var newUser = new User
+			try
 			{
-				Name = input.Name,
-				Password = input.Password,
-				EmployeeCardId = input.EmployeeCardId
-			};
-			dBAttendanceContext.Add(newUser);
-			await dBAttendanceContext.SaveChangesAsync();
-			return newUser;
+				var newUser = new User
+				{
+					Name = input.Name,
+					Password = input.Password,
+					EmployeeCardId = input.EmployeeCardId
+				};
+				dBAttendanceContext.Add(newUser);
+				await dBAttendanceContext.SaveChangesAsync();
+				return newUser;
+			}
+			catch (System.Exception e)
+			{
+				throw new QueryException(e.Message);
+			}
 		}
 
 	}
--- Server/Schema/MutationType.cs
+++ Server/Schema/MutationType.cs
@@ -9,9 +9,21 @@ namespace Server.Schema
 		{
 			base.Configure(descriptor);
 
+			descriptor.Field(m => m.AddAttendance(default, default))
+				.Type<AttendanceType>()
+				.Argument("employeeCardId", a => a.Type<NonNullType<StringType>>());
+
+			descriptor.Field(m => m.OutAttendance(default, default))
+				.Type<AttendanceType>()
+				.Argument("attendanceId", a => a.Type<NonNullType<IdType>>());
+
 			descriptor.Field(m => m.AddLicenseType(default, default))
 				.Type<LicenseTypeType>()
 				.Argument("input", a => a.Type<NonNullType<AddLicenseTypeInputType>>());
+
+			descriptor.Field(m => m.AddUser(default, default))
+				.Type<UserType>()
+				.Argument("input", a => a.Type<NonNullType<AddUserInputType>>());
 		}
 	}
 }
