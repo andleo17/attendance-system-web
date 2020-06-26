0a441965bf1027d8b5b42d1145909c33e2049eb7
Andres Baldarrago <andres.gastulo@gmail.com>
2020-06-26 16:46:54 -0500

    Diseñando el item para empleado.

--- client/src/pages/Employee.jsx
+++ client/src/pages/Employee.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../style/App.css';
 import '../style/bootstrap.css';
-import foto from '../recursos/perfil.jpg';
 import { NavLink } from 'react-router-dom';
+import EmployeeItem from '../components/EmployeeItem';
 
 export default function Employee() {
 	return (
@@ -37,123 +37,11 @@ export default function Employee() {
 						</div>
 					</div>
 				</form>
-
-				
-				
-			
-				<br />
-				<div className=' row card-employeed  '>
-
-					<div className='col-lg-3  card-header  text-center align-items-center fondito'>
-						<img
-							src={foto}
-							alt=''
-							className=' rounded-circle  '
-						/>
-					</div>
-					<div className='col-lg-7 p-2'>
-						<h3 className='ml-3'>Paola Cieza Bances</h3>
-
-						<div className='row'>
-							<div className='col-sm-2 pl-3 text-sistema'>
-								<i className='fa fa-id-card  mr-2'></i>
-								<label htmlFor=''> Dni:</label>
-							</div>
-							<div className='col-sm-8 pl-5'>
-								<label htmlFor=''> 75756219</label>
-							</div>
-						</div>
-
-						<div className='row'>
-							<div className='col-sm-2 pl-3 text-sistema'>
-								<i className='fa fa-venus-mars mr-2'></i>
-								<label htmlFor='' className=''>
-									{' '}
-									Sexo:
-								</label>
-							</div>
-							<div className='col-sm-8 pl-5'>
-								<label htmlFor=''> Femenino</label>
-							</div>
-						</div>
-
-						<div className='row'>
-							<div className='col-sm-2 pl-3 text-sistema'>
-								<i className='fa fa-envelope mr-2'></i>
-								<label htmlFor='' className=''>
-									{' '}
-									Correo:
-								</label>
-							</div>
-							<div className='col-sm-8 pl-5'>
-								<label htmlFor=''> paolacieza8@gmail.com</label>
-							</div>
-						</div>
-
-						<div className='row'>
-							<div className='col-sm-2 pl-3 text-sistema'>
-								<i className='fa fa-ban mr-2'></i>
-								<label htmlFor='' className=''>
-									{' '}
-									Estado:
-								</label>
-							</div>
-							<div className='col-sm-8 pl-5'>
-								<label htmlFor=''> Vigente</label>
-							</div>
-						</div>
-					</div>
-					{/* <div className='col-lg-2 d-flex flex-column text-center align-items-center border border-bottom-0 border-top-0 border-right-0'> */}
-					<div className='col-lg-2 p-2 espaciado border border-bottom-0 border-top-0 border-right-0 text-center'>
-						
-						<button className='degradado border-0' style={{width:'100%'}} >
-							<i className='col-2 fa fa-eye '></i>
-							<label
-								htmlFor=''
-								className='col-9 m-0 ml-2 border border-right-0 border-top-0 border-bottom-0'
-							>
-								{' '}
-								Ver
-							</label>
-						</button>{' '}
-						<br />
-						<button className='degradado  border-0' style={{width:'100%'}}>
-							<i className='col-2 fa fa-pencil-alt '></i>
-							<label
-								htmlFor=''
-								className='col-9 m-0 ml-2 border border-right-0 border-top-0 border-bottom-0'
-								style={{width: ''}}
-							>
-								{' '}
-								Modificar
-							</label>
-						</button>{' '}
-						<br />
-						<button className='degradado  border-0 ' style={{width:'100%'}}>
-							<i className='col-2 fas fa-ban '></i>
-							<label
-								htmlFor=''
-								className='col-9 m-0 ml-2 border border-right-0 border-top-0 border-bottom-0'
-							>
-								{' '}
-								Dar baja
-							</label>
-						</button>{' '}
-						<br />
-						<button className='degradado  border-0' style={{width:'100%'}}>
-							<i className='col-2 fa fa-trash-alt '></i>
-							<label
-								htmlFor=''
-								className='col-9 m-0 ml-2  border border-right-0 border-top-0 border-bottom-0'
-							>
-								{' '}
-								Eliminar
-							</label>
-						</button>{' '}
-
-					</div>
+				<div className='row'>
+					{Array(5)
+						.fill(<EmployeeItem />)
+						.map((e) => e)}
 				</div>
-				<br />
 			</div>
 		</div>
 	);
