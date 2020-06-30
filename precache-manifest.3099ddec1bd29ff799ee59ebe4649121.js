2b3bd9c27db7421019b72c830de919b4125cebbc
Andres Baldarrago <andres.gastulo@gmail.com>
2020-06-30 16:48:27 -0500

    Updates

---
+++ precache-manifest.3099ddec1bd29ff799ee59ebe4649121.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef662e930295bed279d1466d0bab1e6",
+    "url": "/attendance-system-web/index.html"
+  },
+  {
+    "revision": "70f2c4bd72a361a8d8cc",
+    "url": "/attendance-system-web/static/css/main.cd336c6e.chunk.css"
+  },
+  {
+    "revision": "8f4e39be74e6d525aa5a",
+    "url": "/attendance-system-web/static/js/2.6b884881.chunk.js"
+  },
+  {
+    "revision": "fd0b87c2b17de3c5d8ecc8f326f1bf5d",
+    "url": "/attendance-system-web/static/js/2.6b884881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f2c4bd72a361a8d8cc",
+    "url": "/attendance-system-web/static/js/main.68203851.chunk.js"
+  },
+  {
+    "revision": "d012ebcef15d4dc9b0cf",
+    "url": "/attendance-system-web/static/js/runtime-main.c08421ab.js"
+  },
+  {
+    "revision": "dbc60235a74d9eb1fe9309544d0e8ad9",
+    "url": "/attendance-system-web/static/media/embarazada.dbc60235.jpg"
+  },
+  {
+    "revision": "a7d330f4d540d95aa74e85256e6c2d61",
+    "url": "/attendance-system-web/static/media/licenciastrabajo1.a7d330f4.jpg"
+  },
+  {
+    "revision": "17ca7094f0e7f065cedbb685be13579d",
+    "url": "/attendance-system-web/static/media/perfil.17ca7094.jpg"
+  }
+]);
