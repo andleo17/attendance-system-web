58f7ba89a9bdd7c052fa6c16836cc5e45abbab60
Andres Baldarrago <andres.gastulo@gmail.com>
2020-07-20 16:35:37 -0500

    Updates

---
+++ precache-manifest.fda33358a1c3417229569eabdc9d4a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a498a707d0a8aec95baac70aebc068",
+    "url": "/attendance-system-web/index.html"
+  },
+  {
+    "revision": "65c5e488f0fc801b2ae8",
+    "url": "/attendance-system-web/static/js/2.feab198d.chunk.js"
+  },
+  {
+    "revision": "fd0b87c2b17de3c5d8ecc8f326f1bf5d",
+    "url": "/attendance-system-web/static/js/2.feab198d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e057793610b5ce7c9089",
+    "url": "/attendance-system-web/static/js/main.b97c1f08.chunk.js"
+  },
+  {
+    "revision": "d012ebcef15d4dc9b0cf",
+    "url": "/attendance-system-web/static/js/runtime-main.c08421ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29267eebc4c737d2856982b3a2208ba4.js"
+  "/attendance-system-web/precache-manifest.fda33358a1c3417229569eabdc9d4a1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/attendance-system-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
